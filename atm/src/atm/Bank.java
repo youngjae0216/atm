fcf120870d3ab16e8f47b2ed754f1171904d9dc0
EZEN <EZEN@DESKTOP-O3PHIST>
2023-03-14 13:05:05 +0900

    Create project ATM

---
+++ atm/src/atm/Bank.java
@@ -0,0 +1,11 @@
+package atm;
+
+public class Bank {
+	
+	private UserManager um;
+	private AccountManager am;
+	
+	
+	// Banking 관련 메소드 
+	
+}
---
+++ atm/src/atm/UserManager.java
@@ -0,0 +1,15 @@
+package atm;
+
+import java.util.ArrayList;
+
+public class UserManager {
+	private static ArrayList<User> list = new ArrayList<User>();
+	
+	// Account ø° ¥Î«—
+	
+	// Create
+	// Read
+	// Update
+	// Delete
+		
+}
